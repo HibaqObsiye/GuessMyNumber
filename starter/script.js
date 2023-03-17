0ba5be6f994ebcded5d3d4ef0f1c1a2dde7334d5
hibaq obsiye <obsiyehibaq@gmail.com>
2023-03-17 17:59:05 +0000

    created showNumber function

--- starter/script.js
+++ starter/script.js
@@ -6,3 +6,16 @@ document.querySelector('.message').textContent = "Correct Number!"
 
 document.querySelector('.number').textContent = 13
 document.querySelector('.score').textContent = 10
+
+// document.querySelector('.guess').value = 33
+
+document.querySelector('.btn.check').addEventListener("click", showNumber)
+
+function showNumber(){
+  const guess = document.querySelector('.guess').value
+  if(!guess){
+    document.querySelector('.message').textContent = "No number"
+  }
+  
+}
+
