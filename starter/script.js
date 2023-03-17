8a8d3f87e0e5c2a1b47e9c7445bc2db31c08a892
hibaq obsiye <obsiyehibaq@gmail.com>
2023-03-17 20:17:37 +0000

    amended number to be checked to an array

--- starter/script.js
+++ starter/script.js
@@ -2,20 +2,34 @@
 
 console.log(document.querySelector('.message').textContent)
 
-document.querySelector('.message').textContent = "Correct Number!"
+// document.querySelector('.message').textContent = "Correct Number!"
+const secretNumber = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+//const valueCheck = document.querySelector('.number').textContent 
 
-document.querySelector('.number').textContent = 13
 document.querySelector('.score').textContent = 10
 
 // document.querySelector('.guess').value = 33
+
+
 
 document.querySelector('.btn.check').addEventListener("click", showNumber)
 
 function showNumber(){
   const guess = document.querySelector('.guess').value
+  
   if(!guess){
     document.querySelector('.message').textContent = "No number"
+  } 
+ 
+  else if(guess > secretNumber[secretNumber.length -1]){
+    document.querySelector('.message').textContent = "too high"  
   }
-  
+  else if(guess < secretNumber[0]){
+    document.querySelector('.message').textContent = "too low" 
+  }
+  else{
+    document.querySelector('.message').textContent = "Correct Number" 
+  }
 }
+
 
