caa36113d7a6484c699ae7d2e4d16faf43fb6316
hibaq obsiye <obsiyehibaq@gmail.com>
2023-03-21 18:02:40 +0000

    decrease score value

--- starter/script.js
+++ starter/script.js
@@ -3,12 +3,13 @@
 console.log(document.querySelector('.message').textContent)
 
 // document.querySelector('.message').textContent = "Correct Number!"
-const secretNumber = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-//const valueCheck = document.querySelector('.number').textContent 
-
+const secretNumber = Math.trunc(Math.random() * 2) + 1;
+const valueCheck = document.querySelector('.number').textContent  = secretNumber
+let score = document.querySelector('.score').textContent
+  
 document.querySelector('.score').textContent = 10
 
-// document.querySelector('.guess').value = 33
+ document.querySelector('.guess').value = 33
 
 
 
@@ -22,16 +23,21 @@ function showNumber(){
     document.querySelector('.message').textContent = "No number"
   } 
  
-  else if(guess > secretNumber[secretNumber.length -1]){
+  else if(guess > secretNumber){
     document.querySelector('.message').textContent = "too high"  
+    score--
+    document.querySelector('.score').textContent = score
   }
-  else if(guess < secretNumber[0]){
+  else if(guess < secretNumber){
     document.querySelector('.message').textContent = "too low" 
+    score--
+    document.querySelector('.score').textContent = score
   }
   else{
     document.querySelector('.message').textContent = "Correct Number" 
   }
 }
+
 
 function clearNumber(){
     document.querySelector(".guess").value = ""
